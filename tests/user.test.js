ab2201729d3ddc703bffa1a26c4e6fdb4a1f2172
Devdeep Shetranjiwala <devdeepshetranjiwala@gmail.com>
2023-04-21 23:34:16 +0530

    Update user.test.js

--- tests/user.test.js
+++ tests/user.test.js
@@ -5,7 +5,7 @@ const { ApolloClient, InMemoryCache } = require("@apollo/client");
 
 // intialize apollo client
 const client = new ApolloClient({
-  uri: "http://127.0.0.1:5000",
+  uri: "http://127.0.0.1",
   cache: new InMemoryCache(),
 });
 
